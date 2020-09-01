8181e13783f21c89e42a442aec27749585f45a38
Cristián Romo <cristian.g.romo@gmail.com>
2020-09-01 10:25:14 -0500

    Add type-safe L-system definition
    Note to self: I really need to refresh my understanding on closures and
    smart pointers. The type for productions is pretty messy and probably
    not ideal.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use std::option::*;
+use std::rc::Rc;
 
 fn main() {
     println!("Hello, world!");
@@ -10,6 +10,72 @@ fn main() {
         step(&step(axiom)),
         step(&step(&step(axiom)))
     );
+    let algae = LSystem::new(
+        vec![Algae::A],
+        vec![
+            Rc::new(|s| match s {
+                Algae::A => Some(vec![Algae::A, Algae::B]),
+                _ => None,
+            }),
+            Rc::new(|s| match s {
+                Algae::B => Some(vec![Algae::A]),
+                _ => None,
+            }),
+        ],
+    );
+    println!(
+        "Axiom: {:?}, P1: {:?}, P2: {:?}, P3: {:?}",
+        algae.symbols,
+        algae.apply().symbols,
+        algae.apply().apply().symbols,
+        algae.apply().apply().apply().symbols
+    );
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Algae {
+    A,
+    B,
+}
+
+struct LSystem<Alphabet> {
+    symbols: Vec<Alphabet>,
+    productions: Vec<Rc<dyn Fn(&Alphabet) -> Option<Vec<Alphabet>>>>,
+}
+
+impl<Alphabet: Clone + Copy> LSystem<Alphabet> {
+    fn new(
+        axiom: Vec<Alphabet>,
+        productions: Vec<Rc<dyn Fn(&Alphabet) -> Option<Vec<Alphabet>>>>,
+    ) -> LSystem<Alphabet> {
+        LSystem::<Alphabet> {
+            symbols: axiom,
+            productions,
+        }
+    }
+
+    fn apply(&self) -> LSystem<Alphabet> {
+        LSystem::<Alphabet> {
+            symbols: self
+                .symbols
+                .iter()
+                .flat_map(|symbol| step2(symbol, &self.productions))
+                .collect(),
+            productions: self.productions.clone(),
+        }
+    }
+}
+
+fn step2<Alphabet: Copy>(
+    symbol: &Alphabet,
+    productions: &Vec<Rc<dyn Fn(&Alphabet) -> Option<Vec<Alphabet>>>>,
+) -> Vec<Alphabet> {
+    productions
+        .iter()
+        .map(|p| p(symbol))
+        .find(|res| res.is_some())
+        .unwrap_or_else(|| Some(vec![*symbol]))
+        .unwrap()
 }
 
 fn grow(s: &str) -> Option<&str> {
@@ -37,18 +103,3 @@ fn step(s: &str) -> String {
         .map(apply_productions)
         .fold(String::new(), |a, b| a + b)
 }
-
-// G = (V, omega, P) where
-// - V is the alphabet
-// - omega is the axiom
-// - P is the production rules
-
-// So the main things to know from this implementation are:
-// 1. The axiom and intermediate sequences should be a collection of symbols
-// 2. The productions should return an optional list of symbols
-// 3. The `apply_productions` should take a collection of productions to apply
-// 4. The above combine to make the main state needed to track the starting
-//    point or any state thereafter is a collection of symbols and a collection
-//    of productions.
-// 5. The alphabet can be specified as a type for compile-time specified
-//    L-systems.
