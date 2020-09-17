f38624ed181a84b0c55ccf4e0bf30838f19db86d
Cristián Romo <cristian.g.romo@gmail.com>
2020-09-17 13:15:29 -0500

    Add additional demos to the executable

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,63 @@ fn main() {
         ],
     );
     println!(
-        "Axiom: {:?}, P1: {:?}, P2: {:?}, P3: {:?}",
+        "Algae:\nAxiom: {:?}\nP1: {:?}\nP2: {:?}\nP3: {:?}",
         algae.symbols,
         algae.apply().symbols,
         algae.apply().apply().symbols,
         algae.apply().apply().apply().symbols
+    );
+
+    let fractal_tree = aristid::LSystem::new(
+        vec![BinaryTree::Zero],
+        vec![
+            &|s| match s {
+                BinaryTree::One => Some(vec![BinaryTree::One, BinaryTree::One]),
+                _ => None,
+            },
+            &|s| match s {
+                BinaryTree::Zero => Some(vec![
+                    BinaryTree::One,
+                    BinaryTree::Push,
+                    BinaryTree::Zero,
+                    BinaryTree::Pop,
+                    BinaryTree::Zero,
+                ]),
+                _ => None,
+            },
+        ],
+    );
+    println!(
+        "Binary tree:\nAxiom: {:?}\nP1: {:?}\nP2: {:?}\nP3: {:?}",
+        fractal_tree.symbols,
+        fractal_tree.apply().symbols,
+        fractal_tree.apply().apply().symbols,
+        fractal_tree.apply().apply().apply().symbols
+    );
+
+    let koch_curve = aristid::LSystem::new(
+        vec![KochCurve::F],
+        vec![&|s| match s {
+            KochCurve::F => Some(vec![
+                KochCurve::F,
+                KochCurve::Plus,
+                KochCurve::F,
+                KochCurve::Minus,
+                KochCurve::F,
+                KochCurve::Minus,
+                KochCurve::F,
+                KochCurve::Plus,
+                KochCurve::F,
+            ]),
+            _ => None,
+        }],
+    );
+    println!(
+        "Koch curve:\nAxiom: {:?}\nP1: {:?}\nP2: {:?}\nP3: {:?}",
+        koch_curve.symbols,
+        koch_curve.apply().symbols,
+        koch_curve.apply().apply().symbols,
+        koch_curve.apply().apply().apply().symbols
     );
 }
 
@@ -25,4 +77,19 @@ fn main() {
 enum Algae {
     A,
     B,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum BinaryTree {
+    Zero,
+    One,
+    Push,
+    Pop,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum KochCurve {
+    F,
+    Plus,
+    Minus,
 }
