2a2893c526dd0cc4cd54049845205da8e952e650
Cristián Romo <cristian.g.romo@gmail.com>
2020-09-17 10:30:31 -0500

    Add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ impl<Alphabet: Clone + Copy> LSystem<Alphabet> {
   }
 }
 
-pub fn step<Alphabet: Copy>(
+fn step<Alphabet: Copy>(
   symbol: &Alphabet,
   productions: &Vec<Rc<dyn Fn(&Alphabet) -> Option<Vec<Alphabet>>>>,
 ) -> Vec<Alphabet> {
@@ -38,4 +38,103 @@ pub fn step<Alphabet: Copy>(
     .find(|res| res.is_some())
     .unwrap_or_else(|| Some(vec![*symbol]))
     .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+  enum Alphabet {
+    A,
+    B,
+    C,
+  }
+
+  #[test]
+  fn no_applicable_rules_leaves_passes_original_symbol() {
+    let system = LSystem::new(
+      vec![Alphabet::A],
+      vec![Rc::new(|s| match s {
+        Alphabet::B => Some(vec![Alphabet::C]),
+        _ => None,
+      })],
+    );
+    assert_eq!(vec![Alphabet::A], system.apply().symbols);
+  }
+
+  #[test]
+  fn matching_rule_is_applied() {
+    let system = LSystem::new(
+      vec![Alphabet::A],
+      vec![Rc::new(|s| match s {
+        Alphabet::A => Some(vec![Alphabet::B]),
+        _ => None,
+      })],
+    );
+    assert_eq!(vec![Alphabet::B], system.apply().symbols);
+  }
+
+  #[test]
+  fn first_matching_rule_is_applied() {
+    let system = LSystem::new(
+      vec![Alphabet::A],
+      vec![
+        Rc::new(|s| match s {
+          Alphabet::A => Some(vec![Alphabet::B]),
+          _ => None,
+        }),
+        Rc::new(|s| match s {
+          Alphabet::A => Some(vec![Alphabet::C]),
+          _ => None,
+        }),
+      ],
+    );
+    assert_eq!(vec![Alphabet::B], system.apply().symbols);
+  }
+
+  #[test]
+  fn symbols_can_be_replaced_with_multiple_symbols() {
+    let system = LSystem::new(
+      vec![Alphabet::A],
+      vec![Rc::new(|s| match s {
+        Alphabet::A => Some(vec![Alphabet::B, Alphabet::C]),
+        _ => None,
+      })],
+    );
+    assert_eq!(vec![Alphabet::B, Alphabet::C], system.apply().symbols);
+  }
+
+  #[test]
+  fn symbols_can_be_relpaced_with_no_symbols() {
+    let system = LSystem::new(
+      vec![Alphabet::A],
+      vec![Rc::new(|s| match s {
+        Alphabet::A => Some(vec![]),
+        _ => None,
+      })],
+    );
+    assert_eq!(Vec::<Alphabet>::new(), system.apply().symbols);
+  }
+
+  #[test]
+  fn multiple_replacements_happen_in_parallel() {
+    let system = LSystem::new(
+      vec![Alphabet::A, Alphabet::B, Alphabet::A],
+      vec![
+        Rc::new(|s| match s {
+          Alphabet::A => Some(vec![Alphabet::B]),
+          _ => None,
+        }),
+        Rc::new(|s| match s {
+          Alphabet::B => Some(vec![Alphabet::C]),
+          _ => None,
+        }),
+      ],
+    );
+    assert_eq!(
+      vec![Alphabet::B, Alphabet::C, Alphabet::B],
+      system.apply().symbols
+    );
+  }
 }
